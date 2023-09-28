064288f0fb8296507bd18cdc9319647ceb316a30
Dan Brook <dan@broquaint.com>
2023-09-28 13:45:54 +0700

    Keep butterflies well stocked
    Only 3 at a time seems fine. Hopefully.

--- scripts/MaintainHydroponics.gd
+++ scripts/MaintainHydroponics.gd
@@ -5,17 +5,19 @@ onready var butterfly_scene = preload("res://scenes/Butterfly.tscn")
 var butterflies = []
 
 func _ready():
-	remove_child($Butterfly)
 	for i in range(1, 4):
-		var bf = butterfly_scene.instance()
-		bf.get_node("EscapeWindow").connect("timeout", self, "caught_butterfly", [bf])
-		bf.position = Vector2((i+1) * 100 + randi() % 50, (i+1) * 100 + randi() % 50)
-		add_child(bf)
-		butterflies.append(bf)
+		add_butterfly(Vector2((i+1) * 100, (i+1) * 100))
 
 	$NetForButterflies.position = Vector2(100, 100)
 	$NetForButterflies/Catchment.connect("body_entered", self, "catching_butterfly")
 	$NetForButterflies/Catchment.connect("body_exited", self, "losing_butterfly")
+
+func add_butterfly(offset):
+	var bf = butterfly_scene.instance()
+	bf.get_node("EscapeWindow").connect("timeout", self, "caught_butterfly", [bf])
+	bf.position = Vector2(offset.x + randi() % 50, offset.y + randi() % 50)
+	add_child(bf)
+	butterflies.append(bf)
 
 func catching_butterfly(butterfly):
 	if not(butterfly is Butterfly):
@@ -30,11 +32,17 @@ func losing_butterfly(butterfly):
 func caught_butterfly(butterfly):
 	butterfly.get_node('Collision').disabled = true
 	var sprite = butterfly.get_node('Sprite')
+
 	var sb : Tween = butterfly.get_node('Shrink')
 	sb.interpolate_property(
 		sprite, 'scale', sprite.scale, Vector2.ZERO, 0.5, Tween.TRANS_EXPO, Tween.EASE_IN
 	)
 	sb.start()
 	yield(sb, "tween_completed")
+
 	butterflies.erase(butterfly)
 	butterfly.queue_free()
+
+	var x = 400 + randi() % 150 * (-1 if randi() % 2 == 0 else 1)
+	var y = 300 + randi() % 150 * (-1 if randi() % 2 == 0 else 1)
+	add_butterfly(Vector2(x, y))
