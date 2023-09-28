ca60cb869b1a5624bcfd1479bb702678a390663f
Dan Brook <dan@broquaint.com>
2023-09-28 22:24:22 +0700

    Make measures more legible
    With a view to making them more useful too.

--- scripts/Measures.gd
+++ scripts/Measures.gd
@@ -1,5 +1,10 @@
 extends VBoxContainer
 
+func _ready():
+	for child in get_children():
+		if child is SplitContainer:
+			child.get_node('Name').text = child.name
+
 func tick_update_measures(_clock_time: int, _delta: int):
 	update_measures()
 
@@ -7,6 +12,6 @@ func change_update_measures():
 	update_measures()
 
 func update_measures():
-	$EnergyMeter.rect_size.x = 128 * (Habitat.energy_percent() / 100)
-	$TerrariumMeter.rect_size.x = 128 * (Habitat.terrarium_percent() / 100)
+	$Energy/Value/Current.rect_size.x = 128 * (Habitat.energy_percent() / 100)
+	$Terrarium/Value/Current.rect_size.x = 128 * (Habitat.terrarium_percent() / 100)
 	#	$LifeSystemsMeter.rect_size.x = 128 * (Habitat.life_systems / 100)
