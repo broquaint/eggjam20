b9c78a322d27f9515a0d1bb71e2a61bd0795bccf
Dan Brook <dan@broquaint.com>
2023-09-29 23:27:20 +0700

    Implement game bit of life systems check
    It ain't pretty but it works.

--- scripts/CheckLifeSystems.gd
+++ scripts/CheckLifeSystems.gd
@@ -6,11 +6,25 @@ var lanes = [118, 222, 326]
 var switch_lanes = [116, 220, 324]
 var all_switches = []
 
+onready var switch_scene = load("res://scenes/LifeSystemSwitch.tscn")
+
+func make_switch(system, is_on):
+	var switch : LifeSystemSwitch = switch_scene.instance()
+	switch.system = system
+	switch.is_on = is_on
+	return switch
+
 func _ready():
 	randomize()
 	$Bot.position = Vector2(192, lanes[current_lane])
-	$Bot.playing = true
-	var switches = [$H2oOn, $H2oOff, $O2On, $O2Off]
+	$Bot/Sprite.playing = true
+#	var switches = [$H2oOn, $H2oOff, $O2On, $O2Off]
+	var switches = [
+		make_switch('h2o', true),
+		make_switch('h2o', false),
+		make_switch('o2', true),
+		make_switch('o2', false)
+	]
 	var bag = switches.duplicate()
 	for i in range(1,40):
 		var rand_switch = bag[randi() % bag.size()]
@@ -18,7 +32,12 @@ func _ready():
 		var new_switch = rand_switch.duplicate()
 		new_switch.position = Vector2(256 + (256 * i), switch_lanes[randi() % switch_lanes.size()])
 		new_switch.visible = true
+		new_switch.connect('area_entered', self, 'switch_entered', [new_switch])
+		new_switch.connect('area_exited', self, 'switch_exited', [new_switch])
+		# TODO make use of this signal for live updates
+		# new_switch.connect('switch_flipped', self, 'system_change')
 		add_child(new_switch)
+		move_child(new_switch, 2)
 		all_switches.append(new_switch)
 
 		bag.erase(rand_switch)
@@ -43,3 +62,11 @@ func animate_lane_switch():
 		0.5, Tween.TRANS_BACK, Tween.EASE_OUT
 	)
 	$SwitchLane.start()
+
+func switch_entered(node, switch):
+	if node == $Bot:
+		switch.bot_entered()
+
+func switch_exited(node, switch):
+	if node == $Bot:
+		switch.bot_left()
---
+++ scripts/LifeSystemSwitch.gd
@@ -0,0 +1,43 @@
+class_name LifeSystemSwitch
+
+extends Area2D
+
+signal switch_flipped()
+
+var o2_on   : Texture = load("res://assets/images/o2 on.png")
+var o2_off  : Texture = load("res://assets/images/o2 off.png")
+var h2o_on  : Texture = load("res://assets/images/h2o on.png")
+var h2o_off : Texture = load("res://assets/images/h2o off.png")
+
+export var is_on : bool
+export var system : String
+
+var normal :  Texture
+var inverse : Texture
+
+func _ready():
+	match system:
+		'o2':
+			self.normal = o2_on if is_on else o2_off
+			self.inverse = o2_off if is_on else o2_on
+			self.modulate = Color('#99e550') if is_on else Color('#ac3232')
+			$Sprite.texture = normal
+		'h2o':
+			self.normal = h2o_on if is_on else h2o_off
+			self.inverse = h2o_off if is_on else h2o_on
+			self.modulate = Color('#99e550') if is_on else Color('#ac3232')
+			$Sprite.texture = normal
+
+	$DoSwitch.connect('timeout', self, 'switch_flipped')
+
+func bot_entered():
+	$DoSwitch.start()
+func bot_left():
+	$DoSwitch.stop()
+
+func switch_flipped():
+	$Sprite.texture = self.inverse
+	is_on = not is_on
+	self.modulate = Color('#99e550') if is_on else Color('#ac3232')
+	emit_signal("switch_flipped")
+	print("switch ", system, " was ", not is_on, " now ", is_on)
